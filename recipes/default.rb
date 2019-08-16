80747172b61d126985825180ccd8f89d3039275d
mujeebullah <mujeebullahn@gmail.com>
2019-08-16 15:55:39 +0100

    passed unit test for reverse proxy and symbolic link nginx

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,3 +17,22 @@ end
 service 'nginx' do
   action :start
 end
+
+service 'nginx' do
+  action :start
+end
+
+template '/etc/nginx/sites-available/proxy.conf' do
+  source 'proxy.conf.erb'
+  variables proxy_port: node['nginx']['proxy_port']  #take this proxy_port along with proxy.conf file
+  notifies :restart, 'service[nginx]'  #to restart nginx once proxy_port is changed
+end
+
+link '/etc/nginx/sites-enabled/proxy.conf' do
+  to '/etc/nginx/sites-available/proxy.conf'
+end
+
+link '/etc/nginx/sites-enabled/default' do
+  action :delete
+  notifies :restart, 'service[nginx]'  #restart nginx when provision file is changed
+end
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -16,10 +16,6 @@ describe 'nginx::default' do
       expect { chef_run }.to_not raise_error
     end
 
-    it 'run apt-get update' do
-      expect(chef_run).to update_apt_update 'update_sources'
-    end
-
     it 'should install nginx' do
       expect(chef_run).to install_package "nginx"  #run chef with recipe and check if there is nginx installed
     end
@@ -32,5 +28,26 @@ describe 'nginx::default' do
       expect(chef_run).to start_service "nginx"
     end
 
+    it 'it should create a proxy.conf template in /etc/nginx/sites-available' do
+      expect(chef_run).to create_template('/etc/nginx/sites-available/proxy.conf').with_variables(proxy_port:3000)
+    end
+
+    it 'it should create a proxy.conf template in /etc/nginx/sites-available' do
+      expect(chef_run).to create_template('/etc/nginx/sites-available/proxy.conf').with_variables(proxy_port:3000)
+    end
+
+    it 'it should create symlink of sites-available to sites-enabled' do
+      expect(chef_run).to create_link('/etc/nginx/sites-enabled/proxy.conf').with_link_type(:symbolic)
+    end
+
+    it 'should delete the symlink from the defualt config in sites-enabled' do
+      expect(chef_run).to delete_link('/etc/nginx/sites-enabled/default')
+    end
+
+    it 'run apt-get update' do
+      expect(chef_run).to update_apt_update 'update_sources'
+    end
+
+
   end
-  end
+end
